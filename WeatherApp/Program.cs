349b9a9d008da259f6f1871cc9383eb7048b3733
Baltej <baltej.singh@microlise.com>
2023-05-17 15:52:39 +0100

    Update Program.cs

--- WeatherApp/Program.cs
+++ WeatherApp/Program.cs
@@ -30,11 +30,8 @@ builder.Logging.AddOpenTelemetry(options => OpenTelemetryUtil.BuildOpenTelemetry
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
